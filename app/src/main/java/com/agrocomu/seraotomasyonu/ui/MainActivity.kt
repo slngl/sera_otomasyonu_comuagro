acfa7f6a6db3208251d5b3b036b30ce975c5e12f
SELINGUL <dselin26@gmail.com>
2021-03-12 16:43:17 +0300

    bottom navigation bar added

--- app/src/main/java/com/agrocomu/seraotomasyonu/ui/MainActivity.kt
+++ app/src/main/java/com/agrocomu/seraotomasyonu/ui/MainActivity.kt
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.agrocomu.seraotomasyonu.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> {
+                    title=resources.getString(R.string.main_page)
+//                    loadFragment()
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.home2 -> {
+                    title=resources.getString(R.string.main_page)
+//                    loadFragment()
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
